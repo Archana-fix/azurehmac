c823196cb5a75cb327427a2249afdfcec75e310f
archana.stsintern <archana.stsintern@gmail.com>
2024-07-29 17:14:19 +0530

    function inside controller

--- Controller/Function2.cs
+++ Controller/Function2.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace FunctionApp1
+namespace FunctionApp1.Controller
 {
     public static class Function2
     {
